1107e778cab6487bf8315d67ba8cfe3cd303f2ed
Teuku Ghaisa Aufa <tg.aufa@gmail.com>
2023-05-30 04:03:13 +0700

    reorganize import module

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,26 +13,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableInferredTarget
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -40,16 +38,6 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.max
 
 
 class MainActivity : ComponentActivity() {
@@ -125,13 +113,11 @@ fun ArtSpaceLayout(modifierParam: Modifier = Modifier){
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
             Box(
                 modifier = Modifier.weight(4f),
                 contentAlignment = Alignment.BottomCenter) {
                 ArtWorkWall(stepRemember = stepRemember, imgRes = imgResource)
             }
-            Spacer(modifier = Modifier.height(15.dp))
             Box(
                 modifier = Modifier.weight(2f),
                 contentAlignment = Alignment.TopCenter){
@@ -218,7 +204,8 @@ fun ButtonController(onNext: () -> Unit, onPrev: () -> Unit){
     Row(modifier = Modifier
         .fillMaxWidth()
         .padding(top = 5.dp, start = 30.dp, end = 30.dp, bottom = 5.dp),
-        horizontalArrangement = Arrangement.SpaceBetween) {
+        horizontalArrangement = SpaceBetween
+    ) {
         OutlinedButton(
             onClick = onPrev,
             modifier = Modifier.width(150.dp),
